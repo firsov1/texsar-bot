d963314dd9d023d1d0b8c4f002438b4f56301876
Firsov Ivan <firsov@texsar.net>
2024-08-09 14:42:18 +0300

    Second commit. The first working version of the telegram bot. It uses libraries: gspread, telebot

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import gspread
 import telebot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+from datetime import datetime
+
+findsku = 0
 
 bot = telebot.TeleBot("6804968897:AAGykUe9O9eZ0S__H4Z911YtJF1f8XWbrUE")
 
@@ -23,6 +27,46 @@ def main(message):
         'С уважением ООО "Текссар", тел. 8 800 600 74 66, info@texsar.net',
         reply_markup=markup,
     )
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+# @bot.message_handler(func=lambda message: True)
+# def echo_message(message):
+#     bot.reply_to(message, message.text)
+
+
+@bot.message_handler(regexp="ЗАПРОС ОДНОГО АРТИКУЛА")
+def find_sku(message):
+    bot.send_message(message.chat.id, 'Введите артикул:')
+    global findsku
+    findsku = 1
+
+
+@bot.message_handler()
+def check_sku(message):
+    global findsku
+    if findsku == 1:
+        bot.send_message(message.chat.id, 'Один момент, я уже ищу...')
+        sku = message.text
+        # bot.send_message(message.chat.id, 'find sku subroutine check')
+        gc = gspread.service_account()
+        sh = gc.open("1")
+        cell = sh.sheet1.find(sku)
+        if cell is None:
+            bot.send_message(message.chat.id,f'Товара с артикулом {sku} нет наличии. О возможности поставки уточните, пожалуйста, у менеджера по почте info@texsar.ru, либо по телефону 8 800 600 74 66')
+        else:
+            sku = sh.sheet1.cell(cell.row, cell.col).value
+            name = sh.sheet1.cell(cell.row, cell.col + 1).value
+            price = sh.sheet1.cell(cell.row, cell.col + 3).value
+            quantity = sh.sheet1.cell(cell.row, cell.col + 2).value
+            now = datetime.now()
+            print("Found something at R%sC%s" % (cell.row, cell.col))
+            # print(f'{sku} {name} в наличии в количестве {quantity} шт.. \nЦена по прайсу на {now} равна {price} руб.. Цена включает НДС.')
+            # bot.send_message(sku + ' ' + name + 'в наличии в количестве ' + quantity + 'шт.. \nЦена по прайсу на ' + now + 'равна ' + price +' руб.. Цена включает НДС.')
+            bot.send_message(message.chat.id, f'{sku} {name} в наличии в количестве {quantity} шт.. \nЦена по прайсу на {now.day}.{now.month}.{now.year} равна {price} руб.. Цена включает НДС.')
+        findsku = 0
+    else:
+        bot.send_message(message.chat.id, 'Эта кнопка еще не задействована')
 
 
 bot.polling(none_stop=True,timeout=120)
